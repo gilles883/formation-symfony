cd279c64022793a437c41990b915c1bf1a168dd0
Gilles <leonard.gil@free.fr>
2020-09-09 20:54:59 +0200

    sous formulaire

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,17 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Entity\Image;
 use App\Form\AdType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdController extends AbstractController
 {
     /**
      * @Route("/ads", name="ads_index")
-     */
+    */
     public function index(AdRepository $repo)
     {
         $ads = $repo->findAll();
@@ -29,14 +32,32 @@ class AdController extends AbstractController
     *
     * @return Response
     */
-    public function create()
+    public function create(Request $request, EntityManagerInterface $manager)
     {
         $ad = new Ad();
 
         $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($ad);
+            $manager->flush();
+            
+            $this->addFlash(
+                'success',
+                "L'annonce <strong> {$ad->getTitle()}</strong> a bien été enregistrée!"
+            );
+            
+            return $this->redirectToRoute('ads_show', 
+                        [ 'slug' => $ad->getSlug() ]
+            );
+        }
                     
         return $this->render('ad/new.html.twig',
-            [ 'form' => $form->createView() ]);
+            [ 'form' => $form->createView() ]
+        );
     }
     
     /**
